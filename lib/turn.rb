d7234a6a6d84b222efd4f1f87d76d0fda1685618
Felice Forby <feliceforby@gmail.com>
2018-02-05 23:13:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def move(board, position, char="X")
 end
 
 def valid_move?(board, index)
-  !index.between?(0,8) || position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
